2a2ae2a20c6f6cd7475cd3bb60f2cd07e31fe40f
Matous Hybl <hyblmatous@gmail.com>
2020-05-19 22:23:09 +0200

    Modify xd87 keymap.

--- keyboards/handwired/onekey/rules.mk
+++ keyboards/handwired/onekey/rules.mk
@@ -19,5 +19,5 @@ AUDIO_ENABLE = no           # Audio output on port C6
 FAUXCLICKY_ENABLE = no      # Use buzzer to emulate clicky switches
 HD44780_ENABLE = no         # Enable support for HD44780 based LCDs (+400)
 
-DEFAULT_FOLDER = handwired/onekey/promicro
+DEFAULT_FOLDER = handwired/onekey/bluepill
 LAYOUTS = ortho_1x1
