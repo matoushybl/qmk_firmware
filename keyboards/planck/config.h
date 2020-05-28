cd560c1d987385d554262d82189cee26c9ed9a22
Matous Hybl <hyblmatous@gmail.com>
2020-05-28 21:39:21 +0200

    Add handwired maxik based on the black pill.

--- keyboards/planck/config.h
+++ keyboards/planck/config.h
@@ -32,19 +32,19 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MATRIX_COLS 12
 
 /* Planck PCB default pin-out */
-#define MATRIX_ROW_PINS { D0, D5, B5, B6 }
-#define MATRIX_COL_PINS { F1, F0, B0, C7, F4, F5, F6, F7, D4, D6, B4, D7 }
+#define MATRIX_ROW_PINS { D7, C6, D2, D3 }
+#define MATRIX_COL_PINS { C7, F7, F6, F5, F4, F1, F0, D6, B7, B6, B4, E6 }
 #define UNUSED_PINS
 
-#define QMK_ESC_OUTPUT F1
-#define QMK_ESC_INPUT D5
-#define QMK_LED     E6
-#define QMK_SPEAKER C6
-
-#define AUDIO_VOICES
-#define C6_AUDIO
-
-#define BACKLIGHT_PIN B7
+// #define QMK_ESC_OUTPUT F1
+// #define QMK_ESC_INPUT D5
+// #define QMK_LED     E6
+// #define QMK_SPEAKER C6
+//
+// #define AUDIO_VOICES
+// #define C6_AUDIO
+//
+// #define BACKLIGHT_PIN B7
 
 /* COL2ROW or ROW2COL */
 #define DIODE_DIRECTION COL2ROW
