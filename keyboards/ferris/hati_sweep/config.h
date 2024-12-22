09aa42464c4cb3f69f213a00bd804f20169a7adc
Matous Hybl <hyblmatous@gmail.com>
2024-12-22 21:51:21 +0100

    sweep

---
+++ keyboards/ferris/hati_sweep/config.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#define MASTER_RIGHT
+
+// To use the handedness pin, resistors need to be installed on the adapter PCB.
+// If so, uncomment the following code, and undefine MASTER_RIGHT above.
+// #define SPLIT_HAND_PIN GP15
+// #define SPLIT_HAND_PIN_LOW_IS_LEFT  // High -> right, Low -> left.
+
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_LED GP17
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 1000U
